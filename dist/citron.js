1bd81d2f58a5ffac19c69ba243e676f558c32d73
talonbraggbot <pvpgamecrash@gmail.com>
2017-03-09 00:47:55 +0000

    Build

--- dist/citron.js
+++ dist/citron.js
@@ -85,6 +85,32 @@
         context.lineWidth = this.lineW;
         context.strokeStyle = this.strokeStyle;
         context.stroke();
+        
+        //Error Handling
+        if(!this.opts) {
+    		console.log(err);
+    	}
+    	else if(!this.xparam) {
+    		console.log(err);
+    	}
+    	else if(!this.yparam) {
+    		console.log(err);
+    	}
+    	else if(!this.wparam) {
+    		console.log(err);
+    	}
+    	else if(!this.hparam) {
+    		console.log(err);
+    	}
+    	else if(!this.fill) {
+    		console.log(err);
+    	}
+    	else if(!this.lineW) {
+    		console.log(err);
+    	}
+    	else if(!this.strokeStyle) {
+    		console.log(err);
+    	}
     }
     
     
